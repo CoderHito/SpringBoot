65bfa727060102c98edc161e0a7a6cb4515d8862
CoderHitol <i@hitol.me>
2018-09-09 00:45:40 +0800

    读取大数据量的excel

--- src/test/java/com/hitol/springboot/test/ExcelTest.java
+++ src/test/java/com/hitol/springboot/test/ExcelTest.java
@@ -1,45 +1,155 @@
 package com.hitol.springboot.test;
 
 import com.hitol.springboot.test.sb.BaseTest;
+import org.apache.poi.ooxml.util.SAXHelper;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.openxml4j.opc.PackageAccess;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.eventusermodel.ReadOnlySharedStringsTable;
+import org.apache.poi.xssf.eventusermodel.XSSFReader;
+import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler;
+import org.apache.poi.xssf.model.StylesTable;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFComment;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.junit.Test;
+import org.junit.Assert;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
-import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.List;
 
-public class ExcelTest extends BaseTest {
+public class ExcelTest {
+    private static File file = new File("aaa.xlsx");
+    private static File out = new File("test.txt");
 
-    public static void main(String[] args) {
-        try {
-            testReadFile();
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws Exception {
+        ExcelUtil excelUtil = new ExcelUtil(file);
+        excelUtil.setOutFile(out);
+        excelUtil.parseExcelBigData();
+    }
+
+    private static void writeFile() throws Exception {
+        SXSSFWorkbook wb = new SXSSFWorkbook(100); // keep 100 rows in memory, exceeding rows will be flushed to disk
+        Sheet sh = wb.createSheet();
+        for (int rownum = 0; rownum < 1000; rownum++) {
+            Row row = sh.createRow(rownum);
+            for (int cellnum = 0; cellnum < 10; cellnum++) {
+                Cell cell = row.createCell(cellnum);
+                String address = new CellReference(cell).formatAsString();
+                cell.setCellValue(address);
+            }
+
+        }
+
+        // Rows with rownum < 900 are flushed and not accessible
+        for (int rownum = 0; rownum < 900; rownum++) {
+            Assert.assertNull(sh.getRow(rownum));
+        }
+
+        // ther last 100 rows are still in memory
+        for (int rownum = 900; rownum < 1000; rownum++) {
+            Assert.assertNotNull(sh.getRow(rownum));
+        }
+
+        FileOutputStream out = new FileOutputStream("sxssf.xlsx");
+        wb.write(out);
+        out.close();
+
+        // dispose of temporary files backing this workbook on disk
+        wb.dispose();
+    }
+
+
+    private static void readFileBigData() throws Exception {
+        long start = System.currentTimeMillis();
+        System.out.println("-------begin-------");
+
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+//        OPCPackage pkg = OPCPackage.open(fileInputStream);
+//        /**
+//         *  创建表阅读器
+//         */
+//        XSSFReader reader = new XSSFReader(pkg);
+//
+//        /**
+//         *  转换指定单元表
+//         */
+//        InputStream sheet = reader.getSheet("rId" + 0);
+//        InputSource sheetSource = new InputSource(sheet);
+//        StylesTable stylesTable = reader.getStylesTable();
+//        ReadOnlySharedStringsTable stringsTable= new ReadOnlySharedStringsTable(pkg);
+
+        OPCPackage pkg = OPCPackage.open(file, PackageAccess.READ);
+        XSSFReader xssfReader = new XSSFReader(pkg);
+
+        StylesTable styles = xssfReader.getStylesTable();
+        ReadOnlySharedStringsTable strings = new ReadOnlySharedStringsTable(pkg);
+        InputStream sheetInputStream = xssfReader.getSheetsData().next();
+
+        processSheet(styles, strings, sheetInputStream);
+
+        System.out.println("success! 用时:" + (System.currentTimeMillis() - start));
+
+    }
+
+    private static XSSFSheetXMLHandler.SheetContentsHandler handler;
+
+    private static void processSheet(StylesTable styles, ReadOnlySharedStringsTable strings, InputStream sheetInputStream) throws Exception {
+        XMLReader sheetParser = SAXHelper.newXMLReader();
+        if (handler != null) {
+            sheetParser.setContentHandler(new XSSFSheetXMLHandler(styles, strings, handler, false));
+        } else {
+            sheetParser.setContentHandler(new XSSFSheetXMLHandler(styles, strings, new SimpleSheetContentsHandler(), false));
+        }
+
+        sheetParser.parse(new InputSource(sheetInputStream));
+    }
+
+    public static class SimpleSheetContentsHandler implements XSSFSheetXMLHandler.SheetContentsHandler {
+        protected List<String> row = new LinkedList<>();
+
+        @Override
+        public void startRow(int rowNum) {
+            row.clear();
+        }
+
+        @Override
+        public void endRow(int rowNum) {
+            System.err.println(rowNum + " : " + row);
+        }
+
+        @Override
+        public void cell(String cellReference, String formattedValue, XSSFComment comment) {
+            row.add(formattedValue);
+        }
+
+        @Override
+        public void headerFooter(String text, boolean isHeader, String tagName) {
+
         }
     }
 
 
     public static void testReadFile() throws Exception {
-        File file = new File("D:\\bbb.xlsx");
+
+
+//        OutputStream outputStream = new FileOutputStream(out);
+
         System.out.println(file.exists());
         InputStream stream = new FileInputStream(file);
         Workbook xssfWorkbook = new XSSFWorkbook(stream);
         Sheet sheet = xssfWorkbook.getSheetAt(0);
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(stream,"UTF-8"));
-//
-//        String line = "";
-//        while ((line = reader.readLine()) != null){
-//            System.out.println(new String(line.getBytes("GB2312"),"UTF-8"));
-//        }
 
-
-        File out = new File("D:\\test.txt");
-        OutputStream outputStream = new FileOutputStream(out);
-
-
-        for (int i = 1; i < 681254; i++) {
+        for (int i = 1; i < sheet.getLastRowNum(); i++) {
             Row row = sheet.getRow(i);
             int name = (int) row.getCell(0).getNumericCellValue();
             String type = row.getCell(2).getStringCellValue();
@@ -61,12 +171,9 @@ public class ExcelTest extends BaseTest {
             sb.append("\n");
             String data = sb.toString();
             byte[] byteData = data.getBytes();
-            outputStream.write(byteData);
+//            outputStream.write(byteData);
             System.out.println(i);
         }
-
         System.out.println("success!");
-
     }
-
 }
---
+++ src/test/java/com/hitol/springboot/test/ExcelUtil.java
@@ -0,0 +1,222 @@
+package com.hitol.springboot.test;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.poi.ooxml.util.SAXHelper;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.openxml4j.opc.PackageAccess;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.eventusermodel.ReadOnlySharedStringsTable;
+import org.apache.poi.xssf.eventusermodel.XSSFReader;
+import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler;
+import org.apache.poi.xssf.model.StylesTable;
+import org.apache.poi.xssf.usermodel.XSSFComment;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import java.io.*;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Excel读取工具
+ *
+ * @author hitol
+ * @date 2018/9/8 下午7:42
+ */
+public class ExcelUtil {
+
+    Logger logger = LoggerFactory.getLogger(ExcelUtil.class);
+
+    private final int DEFAULT_SHEET_INDEX = 0;
+    private final int DEFAULT_SHEET_NUM = 0;
+    private String inFilePath;
+    private String outFilePath;
+    private File inFile;
+    private File outFile;
+    private String inFileName;
+    private String outFileName;
+    private InputStream inputStream;
+    private OutputStream outputStream;
+    private int sheetNum;
+
+    private XSSFSheetXMLHandler.SheetContentsHandler handler;
+
+
+    public ExcelUtil(String inFilePath) throws Exception {
+        if (StringUtils.isEmpty(inFilePath)) {
+            throw new Exception("inFilePath is empty!");
+        }
+        this.inFilePath = inFilePath;
+        this.inFile = new File(inFilePath);
+
+    }
+
+    public ExcelUtil(File inFile) {
+        this.inFile = inFile;
+    }
+
+    public void setSheetNum(int sheetNum) {
+        this.sheetNum = sheetNum;
+    }
+
+    public void setOutFile(File outFile) {
+        this.outFile = outFile;
+        try {
+            this.outputStream = new FileOutputStream(outFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void parseExcelBigData() throws Exception {
+        long start = System.currentTimeMillis();
+        logger.info("-------start-------");
+
+        inputStream = new FileInputStream(inFile);
+        OPCPackage pkg = OPCPackage.open(inFile, PackageAccess.READ);
+        XSSFReader xssfReader = new XSSFReader(pkg);
+
+        StylesTable styles = xssfReader.getStylesTable();
+        ReadOnlySharedStringsTable strings = new ReadOnlySharedStringsTable(pkg);
+        InputStream sheetInputStream = xssfReader.getSheetsData().next();
+
+        processSheet(styles, strings, sheetInputStream);
+
+        logger.info("success! 用时:" + (System.currentTimeMillis() - start));
+    }
+
+    private void processSheet(StylesTable styles, ReadOnlySharedStringsTable strings, InputStream sheetInputStream) throws Exception {
+        XMLReader sheetParser = SAXHelper.newXMLReader();
+        if (handler != null) {
+            sheetParser.setContentHandler(new XSSFSheetXMLHandler(styles, strings, handler, false));
+        } else {
+            sheetParser.setContentHandler(new XSSFSheetXMLHandler(styles, strings, new SimpleSheetContentsHandler(), false));
+        }
+
+        sheetParser.parse(new InputSource(sheetInputStream));
+    }
+
+    //INSERT INTO tm_white_list_info(ORG,CUST_NAME,ID_NO,ID_TYPE,CURRENT_LIMIT) VALUES('9982','16',x,'xk',y);
+    private String sql = "INSERT INTO tm_white_list_info(ORG,CUST_NAME,ID_NO,ID_TYPE,CURRENT_LIMIT) VALUES('9982',";
+
+    private class SimpleSheetContentsHandler implements XSSFSheetXMLHandler.SheetContentsHandler {
+        protected List<String> row = new LinkedList<>();
+
+        @Override
+        public void startRow(int rowNum) {
+            row.clear();
+        }
+
+        @Override
+        public void endRow(int rowNum) {
+            String data = parseRowReturnString(row);
+            System.err.println(rowNum + " : " + data);
+            writeToFile(data);
+        }
+
+
+        @Override
+        public void cell(String cellReference, String formattedValue, XSSFComment comment) {
+            row.add(formattedValue);
+        }
+
+        @Override
+        public void headerFooter(String text, boolean isHeader, String tagName) {
+
+        }
+    }
+
+    private String parseRowReturnString(List<String> row) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(sql).append("\'").append(row.get(0)).append("\'");
+        sb.append("\'").append(row.get(2)).append("\',");
+        sb.append("\'").append(row.get(1)).append("\',");
+        sb.append(row.get(3)).append("); \n");
+//            System.err.println(rowNum + " : " + row);
+        return sb.toString();
+    }
+
+    private void writeToFile(String data) {
+        if (outputStream != null) {
+            byte[] byteData = data.getBytes();
+            try {
+                outputStream.write(byteData);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    /**
+     * 适用于数据量较小
+     */
+    public void parseExcel() throws Exception {
+        if (!inFile.exists()) {
+            throw new Exception(" file is not exist!");
+        }
+
+        inputStream = new FileInputStream(inFile);
+        Workbook workbook = new XSSFWorkbook(inFile);
+        parseWorkBook(workbook);
+
+    }
+
+    private void parseWorkBook(Workbook workbook) {
+        if (sheetNum == 0) {
+            parseSheetOnce(workbook, DEFAULT_SHEET_INDEX);
+        } else {
+            parseSheet(workbook);
+        }
+    }
+
+    private void parseSheet(Workbook workbook) {
+        for (int i = 0; i < sheetNum; i++) {
+            parseSheetOnce(workbook, i);
+        }
+    }
+
+    private void parseSheetOnce(Workbook workbook, int sheetIndex) {
+        Sheet sheet = workbook.getSheetAt(sheetIndex);
+
+        Iterator<Row> rowIterator = sheet.iterator();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            List<String> rowList = new LinkedList<>();
+            Iterator<Cell> cellIterator = row.iterator();
+            int rowIndex = 0;
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                rowIndex = cell.getRowIndex();
+                switch (cell.getCellType()) {
+                    case STRING:
+                        rowList.add(cell.getStringCellValue());
+                        break;
+                    case NUMERIC:
+                        rowList.add(String.valueOf(cell.getNumericCellValue()));
+                        break;
+                    case BOOLEAN:
+                        rowList.add(String.valueOf(cell.getBooleanCellValue()));
+                        break;
+                    case ERROR:
+                        rowList.add(String.valueOf(cell.getErrorCellValue()));
+                        break;
+                    case BLANK:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            System.err.println(rowIndex + ":" + rowList);
+            writeToFile(parseRowReturnString(rowList));
+        }
+
+    }
+}
